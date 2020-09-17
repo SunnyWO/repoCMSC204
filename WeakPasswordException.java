c21f740b8156fa0a75362c5a87ec7d2b8a355c21
SunnyWO <60673125+SunnyWO@users.noreply.github.com>
2020-09-16 21:42:42 -0400

    Delete WeakPasswordException.java

--- WeakPasswordException.java
+++
@@ -1,9 +0,0 @@
-
-public class WeakPasswordException extends Exception {
-	
-	public WeakPasswordException() {
-		super();
-		System.out.println("The password is OK but weak - it contains fewer than 10 characters");
-	}
-
-}
