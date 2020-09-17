a1b6ab8ed95c004cbf039d72c6a406535017daf0
SunnyWO <60673125+SunnyWO@users.noreply.github.com>
2020-09-16 21:41:56 -0400

    Delete NoUpperAlphaException.java

--- NoUpperAlphaException.java
+++
@@ -1,9 +0,0 @@
-
-public class NoUpperAlphaException extends Exception {
-	
-	public NoUpperAlphaException() {
-		super();
-		System.out.println("The password must contain at least one uppercase alphabetic character");
-	}
-
-}
